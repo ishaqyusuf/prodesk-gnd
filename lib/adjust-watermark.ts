7b3b00dd3d1eb3602eabd83791d5703006bc907a
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-07-28 16:52:31 +0100

    :tada:

--- lib/adjust-watermark.ts
+++ lib/adjust-watermark.ts
@@ -2,8 +2,7 @@ import { _useId } from "@/hooks/use-id";
 
 export function adjustWatermark(orderIds: string[]) {
   const w = document.getElementById("waterMark");
-  console.log(w);
-  //   return;
+
   let pages = 0;
   orderIds.map((elementId) => {
     let pgHeight, invoiceHeight;
@@ -11,11 +10,11 @@ export function adjustWatermark(orderIds: string[]) {
     invoiceHeight = 950;
     const selector = (s) => `#s${elementId} ${s}`;
     const header = document.querySelector(selector("#topHeader"));
-    const headerHeight = header?.clientHeight;
+    const headerHeight = header?.clientHeight || 0;
     invoiceHeight -= headerHeight;
     const footer = document.querySelector(selector("#footer"));
-    invoiceHeight -= footer?.clientHeight;
-    console.log(invoiceHeight, pgHeight);
+    invoiceHeight -= footer?.clientHeight || 0;
+
     let totalLineHeight = 0;
     document.querySelectorAll(selector("tbody  tr")).forEach((e, i) => {
       // console.log([e.id, e.clientHeight, th]);
@@ -27,31 +26,32 @@ export function adjustWatermark(orderIds: string[]) {
     });
     const lp = totalLineHeight % invoiceHeight;
     const px = invoiceHeight - lp;
-    const fl: HTMLElement = document.querySelector(selector("div#filler"));
+    const fl = document.querySelector(selector("div#filler"));
     let _pgs = Math.ceil(totalLineHeight / invoiceHeight);
-    console.log({
-      totalLineHeight,
-      invoiceHeight,
-      px,
-      _pgs,
-    });
-    for (let i = 0; i < _pgs; i++) {
-      const n = w.cloneNode(true) as HTMLElement;
-      n.setAttribute("id", _useId("water"));
-      n.classList.remove("hidden");
-      const topPx = Math.ceil(
-        pgHeight * pages + headerHeight + invoiceHeight / 2
-      );
-      // console.log([pgHeight, pages, headerHeight, invoiceHeight]);
-      n.style.top = `${topPx}px`;
-      // console.log([topPx]);
-      w.after(n);
-      pages++;
-    }
-    console.log(pages);
-    if (fl) {
-      const h = `${px}px`;
-      //   fl.style.height = h;
-    }
+    // console.log({
+    //   totalLineHeight,
+    //   invoiceHeight,
+    //   px,
+    //   _pgs,
+    // });
+    if (w)
+      for (let i = 0; i < _pgs; i++) {
+        const n = w.cloneNode(true) as HTMLElement;
+        n.setAttribute("id", _useId("water"));
+        n.classList.remove("hidden");
+        const topPx = Math.ceil(
+          pgHeight * pages + headerHeight + invoiceHeight / 2
+        );
+        // console.log([pgHeight, pages, headerHeight, invoiceHeight]);
+        n.style.top = `${topPx}px`;
+        // console.log([topPx]);
+        w.after(n);
+        pages++;
+      }
+    // console.log(pages);
+    // if (fl) {
+    //   const h = `${px}px`;
+    //   //   fl.style.height = h;
+    // }
   });
 }
