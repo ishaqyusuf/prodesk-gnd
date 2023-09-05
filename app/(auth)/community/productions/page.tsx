137042b6a4bb1e9e32d8b091c3f8e9d11400fb13
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-09-05 14:39:43 +0100

    :tada:

--- app/(auth)/community/productions/page.tsx
+++ app/(auth)/community/productions/page.tsx
@@ -5,10 +5,6 @@ import PageHeader from "@/components/page-header";
 import { Breadcrumbs } from "@/components/breadcrumbs";
 import { BreadLink } from "@/components/breadcrumbs/links";
 
-import { getHomesAction } from "@/app/_actions/community/home";
-import CommunityInvoiceTableShell from "@/components/shells/community-invoice-table-shell";
-import { getInvoices } from "@/app/_actions/community-invoice/get-invoices";
-import EditInvoiceModal from "@/components/modals/edit-invoice-modal";
 import CommunityProductionsTableShell from "@/components/shells/community-productions";
 import { getProductions } from "@/app/_actions/community-production/get-productions";
 
