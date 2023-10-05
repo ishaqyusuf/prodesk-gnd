36ba3c91b84011e71c428487968224451d925e9e
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-10-05 16:59:11 +0100

    :tada:

--- app/_actions/community/builders.ts
+++ app/_actions/community/builders.ts
@@ -7,48 +7,59 @@ import { getPageInfo, queryFilter } from "../action-utils";
 import { IBuilder } from "@/types/community";
 export interface BuildersQueryParams extends BaseQuery {}
 export async function getBuildersAction(query: BuildersQueryParams) {
-  const where = whereBuilder(query);
-  const _items = await prisma.builders.findMany({
-    where,
-    include: {
-      _count: {
-        select: {
-          projects: true,
+    const where = whereBuilder(query);
+    const _items = await prisma.builders.findMany({
+        where,
+        include: {
+            _count: {
+                select: {
+                    projects: true
+                }
+            }
+            //  builder: true,
         },
-      },
-      //  builder: true,
-    },
-    ...(await queryFilter(query)),
-  });
-  const pageInfo = await getPageInfo(query, where, prisma.builders);
+        ...(await queryFilter(query))
+    });
+    const pageInfo = await getPageInfo(query, where, prisma.builders);
 
-  return {
-    pageInfo,
-    data: _items as any,
-  };
+    return {
+        pageInfo,
+        data: _items as any
+    };
 }
 function whereBuilder(query: BuildersQueryParams) {
-  const q = {
-    contains: query._q || undefined,
-  };
-  const where: Prisma.BuildersWhereInput = {
-    // builderId: {
-    //   equals: Number(query._builderId) || undefined,
-    // },
-  };
+    const q = {
+        contains: query._q || undefined
+    };
+    const where: Prisma.BuildersWhereInput = {
+        // builderId: {
+        //   equals: Number(query._builderId) || undefined,
+        // },
+    };
 
-  return where;
+    return where;
 }
 export async function staticBuildersAction() {
-  const _data = await prisma.builders.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-  });
-  return _data;
+    const _data = await prisma.builders.findMany({
+        select: {
+            id: true,
+            name: true
+        }
+    });
+    return _data;
 }
 export async function deleteBuilderAction(id) {}
-export async function saveBuilder(data: IBuilder) {}
-export async function saveBuilderTasks(data: IBuilder) {}
+export async function saveBuilder(data: IBuilder) {
+    // data.createdAt = data.
+}
+export async function saveBuilderTasks(data: IBuilder) {
+    await prisma.builders.update({
+        where: {
+            id: data.id
+        },
+        data: {
+            meta: data.meta as any
+        }
+    });
+}
 export async function saveBuilderInstallations(data: IBuilder) {}
