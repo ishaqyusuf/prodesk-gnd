251aebe0bbb70b1dd7b50fb3410cabd83d9f7cb7
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-10-05 22:05:22 +0100

    :tada:

--- app/_actions/community/home.ts
+++ app/_actions/community/home.ts
@@ -6,153 +6,158 @@ import { dateQuery, getPageInfo, queryFilter } from "../action-utils";
 import { Prisma } from "@prisma/client";
 
 export interface HomeQueryParams extends BaseQuery {
-  _builderId;
-  _projectSlug;
-  _projectId?;
-  _production?: "Started" | "Queued" | "Idle" | "Completed";
-  _installation?: "Submitted" | "No Submission";
+    _builderId;
+    _projectSlug;
+    _projectId?;
+    _production?: "Started" | "Queued" | "Idle" | "Completed";
+    _installation?: "Submitted" | "No Submission";
 }
 export async function getHomesAction(query: HomeQueryParams) {
-  const where = await whereHome(query);
-  const homes = await prisma.homes.findMany({
-    ...(await queryFilter(query)),
-    where,
-    include: {
-      project: {
-        include: {
-          builder: true,
-        },
-      },
-      tasks: {
-        select: {
-          taskUid: true,
-          produceable: true,
-          producedAt: true,
-          installable: true,
-          sentToProductionAt: true,
-          amountDue: true,
-          amountPaid: true,
-        },
-      },
-      jobs: {
-        select: {
-          id: true,
-          createdAt: true,
-        },
-      },
-    },
-  });
-  const pageInfo = await getPageInfo(query, where, prisma.homes);
-  return {
-    pageInfo,
-    data: homes,
-  };
-}
-export async function getProjectHomesAction(query: HomeQueryParams) {
-  const where = await whereHome(query, true);
-  const project = await prisma.projects.findUnique({
-    where: {
-      slug: query._projectSlug,
-    },
-    include: {
-      builder: true,
-      homes: {
+    const where = await whereHome(query);
+    const homes = await prisma.homes.findMany({
         ...(await queryFilter(query)),
         where,
         include: {
-          jobs: {
-            select: {
-              id: true,
-              createdAt: true,
+            project: {
+                include: {
+                    builder: true
+                }
             },
-          },
-          tasks: {
-            select: {
-              taskUid: true,
-              produceable: true,
-              installable: true,
-              producedAt: true,
-              sentToProductionAt: true,
+            tasks: {
+                select: {
+                    taskUid: true,
+                    produceable: true,
+                    producedAt: true,
+                    installable: true,
+                    sentToProductionAt: true,
+                    amountDue: true,
+                    amountPaid: true
+                }
             },
-          },
+            jobs: {
+                select: {
+                    id: true,
+                    createdAt: true
+                }
+            }
+        }
+    });
+    const pageInfo = await getPageInfo(query, where, prisma.homes);
+    return {
+        pageInfo,
+        data: homes
+    };
+}
+export async function getProjectHomesAction(query: HomeQueryParams) {
+    const where = await whereHome(query, true);
+    const project = await prisma.projects.findUnique({
+        where: {
+            slug: query._projectSlug
         },
-      },
-    },
-  });
-  if (!project) throw new Error("Project Not found");
-  const pageInfo = await getPageInfo(
-    query,
-    await whereHome(query),
-    prisma.homes
-  );
-  const { homes, ...pdata } = project as any;
-  return {
-    pageInfo,
-    data: homes.map((home) => {
-      return {
-        ...home,
-        project: pdata,
-      };
-    }),
-    project: pdata,
-  };
+        include: {
+            builder: true,
+            homes: {
+                ...(await queryFilter(query)),
+                where,
+                include: {
+                    jobs: {
+                        select: {
+                            id: true,
+                            createdAt: true
+                        }
+                    },
+                    tasks: {
+                        select: {
+                            taskUid: true,
+                            produceable: true,
+                            installable: true,
+                            producedAt: true,
+                            sentToProductionAt: true
+                        }
+                    }
+                }
+            }
+        }
+    });
+    if (!project) throw new Error("Project Not found");
+    const pageInfo = await getPageInfo(
+        query,
+        await whereHome(query),
+        prisma.homes
+    );
+    const { homes, ...pdata } = project as any;
+    return {
+        pageInfo,
+        data: homes.map(home => {
+            return {
+                ...home,
+                project: pdata
+            };
+        }),
+        project: pdata
+    };
 }
 export async function deleteHome(id) {
-  //delete home along with accessories
-  await prisma.homes.delete({
-    where: {
-      id,
-    },
-    include: {
-      jobs: true,
-      tasks: true,
-    },
-  });
+    //delete home along with accessories
+    await prisma.homes.delete({
+        where: {
+            id
+        },
+        include: {
+            jobs: true,
+            tasks: true
+        }
+    });
 }
 export async function whereHome(query: HomeQueryParams, asInclude = false) {
-  const q: any = {
-    contains: query._q || undefined,
-  };
-  // if (query._q) q.contains = query._q;
+    const q: any = {
+        contains: query._q || undefined
+    };
+    // if (query._q) q.contains = query._q;
 
-  const where: Prisma.HomesWhereInput = {
-    builderId: {
-      equals: Number(query._builderId) || undefined,
-    },
-    ...dateQuery(query),
-  };
-  if (q.contains)
-    where.OR = [
-      {
-        search: q,
-      },
-      {
-        modelName: q,
-      },
-    ];
-  if (!asInclude) {
-    if (query._projectSlug) {
-      where.project = {
-        slug: query._projectSlug,
-      };
+    const where: Prisma.HomesWhereInput = {
+        builderId: {
+            equals: Number(query._builderId) || undefined
+        },
+        // project: {
+        //     id: {
+        //         gt: 0
+        //     }
+        // },
+        ...dateQuery(query)
+    };
+    if (q.contains)
+        where.OR = [
+            {
+                search: q
+            },
+            {
+                modelName: q
+            }
+        ];
+    if (!asInclude) {
+        if (query._projectSlug) {
+            where.project = {
+                slug: query._projectSlug
+            };
+        }
     }
-  }
-  if (query._projectId) where.projectId = Number(query._projectId);
-  switch (query._production) {
-    case "Completed":
-      break;
-    case "Idle":
-      break;
-    case "Queued":
-      break;
-    case "Started":
-      break;
-  }
-  switch (query._installation) {
-    case "No Submission":
-      break;
-    case "Submitted":
-      break;
-  }
-  return where;
+    if (query._projectId) where.projectId = Number(query._projectId);
+    switch (query._production) {
+        case "Completed":
+            break;
+        case "Idle":
+            break;
+        case "Queued":
+            break;
+        case "Started":
+            break;
+    }
+    switch (query._installation) {
+        case "No Submission":
+            break;
+        case "Submitted":
+            break;
+    }
+    return where;
 }
--- app/_actions/sales/sales-payment.ts
+++ app/_actions/sales/sales-payment.ts
@@ -5,152 +5,152 @@ import { toFixed } from "@/lib/use-number";
 import { ISalesPayment } from "@/types/sales";
 import { getCustomerWallet } from "../customer-wallet/wallet";
 import {
-  creditTransaction,
-  debitTransaction,
+    creditTransaction,
+    debitTransaction
 } from "../customer-wallet/transaction";
 import { sum } from "@/lib/utils";
 
 export interface PaymentOrderProps {
-  id;
-  amountDue;
-  amountPaid;
-  customerId;
-  orderId;
-  paymentOption;
-  checkNo;
+    id;
+    amountDue;
+    amountPaid;
+    customerId;
+    orderId;
+    paymentOption;
+    checkNo;
 }
 export interface ApplyPaymentProps {
-  orders: PaymentOrderProps[];
-  credit;
-  debit;
-  balance;
+    orders: PaymentOrderProps[];
+    credit;
+    debit;
+    balance;
 }
 export async function applyPaymentAction({
-  orders,
-  credit,
-  debit,
-  balance,
-}: ApplyPaymentProps) {
-  const wallet = await getCustomerWallet(orders[0]?.customerId);
-  await creditTransaction(wallet.id, credit);
-
-  const transaction = await debitTransaction(
-    wallet.id,
+    orders,
+    credit,
     debit,
-    `Payment for order: ${orders.map((o) => o.orderId)}`
-  );
-  await prisma.salesPayments.createMany({
-    data: orders.map(
-      (o) =>
-        ({
-          transactionId: transaction.id,
-          amount: +o.amountPaid,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          orderId: o.id,
-          customerId: o.customerId,
-          //   order: {
-          //     connect: o.orderId,
-          //   },
-          meta: {
-            paymentOption: o.paymentOption,
-            checkNo: o.checkNo,
-          },
-        } as ISalesPayment)
-    ),
-  });
-  await Promise.all(
-    orders.map(async ({ id, amountDue }) => {
-      await prisma.salesOrders.update({
-        where: {
-          id,
-        },
-        data: {
-          amountDue,
-          updatedAt: new Date(),
-        },
-      });
-    })
-  );
-  return true;
+    balance
+}: ApplyPaymentProps) {
+    const wallet = await getCustomerWallet(orders[0]?.customerId);
+    await creditTransaction(wallet.id, credit, "credit");
+
+    const transaction = await debitTransaction(
+        wallet.id,
+        debit,
+        `Payment for order: ${orders.map(o => o.orderId)}`
+    );
+    await prisma.salesPayments.createMany({
+        data: orders.map(
+            o =>
+                ({
+                    transactionId: transaction.id,
+                    amount: +o.amountPaid,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    orderId: o.id,
+                    customerId: o.customerId,
+                    //   order: {
+                    //     connect: o.orderId,
+                    //   },
+                    meta: {
+                        paymentOption: o.paymentOption,
+                        checkNo: o.checkNo
+                    }
+                } as ISalesPayment)
+        )
+    });
+    await Promise.all(
+        orders.map(async ({ id, amountDue }) => {
+            await prisma.salesOrders.update({
+                where: {
+                    id
+                },
+                data: {
+                    amountDue,
+                    updatedAt: new Date()
+                }
+            });
+        })
+    );
+    return true;
 }
 export async function deleteSalesPayment({
-  id,
-  amount,
-  orderId,
-  amountDue,
-  refund,
-}) {
-  await prisma.salesPayments.delete({
-    where: { id },
-  });
-
-  const sales = await prisma.salesOrders.update({
-    where: {
-      id: orderId,
-    },
-    data: {
-      amountDue,
-    },
-    include: {
-      customer: true,
-    },
-  });
-  const wallet = await getCustomerWallet(sales.customerId);
-  await creditTransaction(
-    wallet.id,
-    refund ? amount : 0,
+    id,
+    amount,
+    orderId,
+    amountDue,
     refund
-      ? `Sales Payment deleted and refunded (${sales.orderId})`
-      : `Sales Payment deleted with no refund (${sales.orderId}). $${amount}`
-  );
+}) {
+    await prisma.salesPayments.delete({
+        where: { id }
+    });
+
+    const sales = await prisma.salesOrders.update({
+        where: {
+            id: orderId
+        },
+        data: {
+            amountDue
+        },
+        include: {
+            customer: true
+        }
+    });
+    const wallet = await getCustomerWallet(sales.customerId);
+    await creditTransaction(
+        wallet.id,
+        refund ? amount : 0,
+        refund
+            ? `Sales Payment deleted and refunded (${sales.orderId})`
+            : `Sales Payment deleted with no refund (${sales.orderId}). $${amount}`
+    );
 }
 export async function fixPaymentAction({
-  amountDue,
-  id,
+    amountDue,
+    id
 }: {
-  id: number;
-  amountDue: number;
+    id: number;
+    amountDue: number;
 }) {
-  await prisma.salesOrders.update({
-    where: { id },
-    data: {
-      amountDue,
-    },
-  });
+    await prisma.salesOrders.update({
+        where: { id },
+        data: {
+            amountDue
+        }
+    });
 }
 export async function fixSalesPaymentAction(id) {
-  const order = await prisma.salesOrders.findUnique({
-    where: {
-      id,
-    },
-    include: {
-      payments: true,
-    },
-  });
-  let totalPaid = 0;
-  order?.payments?.map((p) => {
-    totalPaid += p.amount || 0;
-  });
+    const order = await prisma.salesOrders.findUnique({
+        where: {
+            id
+        },
+        include: {
+            payments: true
+        }
+    });
+    let totalPaid = 0;
+    order?.payments?.map(p => {
+        totalPaid += p.amount || 0;
+    });
 
-  let amountDue = (order?.grandTotal || 0) - totalPaid;
-  await fixPaymentAction({
-    id,
-    amountDue: +toFixed(amountDue),
-  });
+    let amountDue = (order?.grandTotal || 0) - totalPaid;
+    await fixPaymentAction({
+        id,
+        amountDue: +toFixed(amountDue)
+    });
 }
 export async function updatePaymentTerm(id, paymentTerm, goodUntil) {
-  await prisma.salesOrders.update({
-    where: { id },
-    data: {
-      paymentTerm,
-      goodUntil,
-    },
-  });
-  // const d = await prisma.salesOrders.findUnique({
-  //   where: { id },
-  // });
-  // if (!d) throw new Error("Order Not Found");
-  // const meta: ISalesOrderMeta = d.meta as any;
-  // meta.pa
+    await prisma.salesOrders.update({
+        where: { id },
+        data: {
+            paymentTerm,
+            goodUntil
+        }
+    });
+    // const d = await prisma.salesOrders.findUnique({
+    //   where: { id },
+    // });
+    // if (!d) throw new Error("Order Not Found");
+    // const meta: ISalesOrderMeta = d.meta as any;
+    // meta.pa
 }
