4adc8c8f113a86cdc51df56aeb9897c4a24a5d52
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-10-10 17:17:45 +0100

    :tada:

--- components/shells/community-invoice-table-shell.tsx
+++ components/shells/community-invoice-table-shell.tsx
@@ -4,13 +4,13 @@ import { TableShellProps } from "@/types/data-table";
 import { ColumnDef } from "@tanstack/react-table";
 import { useMemo, useState, useTransition } from "react";
 import {
-  CheckColumn,
-  ColumnHeader,
-  Cell,
-  PrimaryCellContent,
-  DateCellContent,
-  SecondaryCellContent,
-  _FilterColumn,
+    CheckColumn,
+    ColumnHeader,
+    Cell,
+    PrimaryCellContent,
+    DateCellContent,
+    SecondaryCellContent,
+    _FilterColumn
 } from "../columns/base-columns";
 
 import { DataTable2 } from "../data-table/data-table-2";
@@ -18,16 +18,16 @@ import { DataTable2 } from "../data-table/data-table-2";
 import { ExtendedHome, IHome } from "@/types/community";
 import { BuilderFilter } from "../filters/builder-filter";
 import {
-  HomeInstallationStatus,
-  HomeInvoiceColumn,
-  HomeProductionStatus,
-  HomeStatus,
+    HomeInstallationStatus,
+    HomeInvoiceColumn,
+    HomeProductionStatus,
+    HomeStatus
 } from "../columns/community-columns";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
 } from "../ui/dropdown-menu";
 import { Button } from "../ui/button";
 import { MoreHorizontal, Printer, View } from "lucide-react";
@@ -38,186 +38,190 @@ import { HomesSelectionAction } from "../community/homes-selection-action";
 import HomePrinter from "../print/home/home-printer";
 import { deepCopy } from "@/lib/deep-copy";
 import {
-  DeleteRowAction,
-  EditRowAction,
+    DeleteRowAction,
+    EditRowAction
 } from "../data-table/data-table-row-actions";
 import { ProjectsFilter } from "../filters/projects-filter";
 import { labelValue } from "@/lib/utils";
 import { openModal } from "@/lib/modal";
 
 export default function HomesTableShell<T>({
-  data,
-  pageInfo,
-  searchParams,
-  projectView,
+    data,
+    pageInfo,
+    searchParams,
+    projectView
 }: TableShellProps<ExtendedHome> & {
-  projectView: Boolean;
+    projectView: Boolean;
 }) {
-  const [isPending, startTransition] = useTransition();
+    const [isPending, startTransition] = useTransition();
 
-  const [selectedRowIds, setSelectedRowIds] = useState<number[]>([]);
-  const columns = useMemo<ColumnDef<ExtendedHome, unknown>[]>(
-    () => [
-      CheckColumn({ selectedRowIds, setSelectedRowIds, data }),
-      {
-        maxSize: 10,
-        id: "id",
-        header: ColumnHeader("ID"),
-        cell: ({ row }) => (
-          <Cell>
-            <PrimaryCellContent>{row.original.id}</PrimaryCellContent>
-            <DateCellContent>{row.original.createdAt}</DateCellContent>
-          </Cell>
-        ),
-      },
-      ...(!projectView
-        ? ([
+    const [selectedRowIds, setSelectedRowIds] = useState<number[]>([]);
+    const columns = useMemo<ColumnDef<ExtendedHome, unknown>[]>(
+        () => [
+            CheckColumn({ selectedRowIds, setSelectedRowIds, data }),
             {
-              header: ColumnHeader("Project"),
-              id: "title",
-              cell: ({ row }) => (
-                <Cell
-                  link="/community/project/slug"
-                  slug={row.original.project?.slug}
-                >
-                  <PrimaryCellContent>
-                    {row.original?.project?.title}
-                  </PrimaryCellContent>
-
-                  <SecondaryCellContent>
-                    {row.original?.project?.builder?.name}
-                  </SecondaryCellContent>
-                </Cell>
-              ),
+                maxSize: 10,
+                id: "id",
+                header: ColumnHeader("ID"),
+                cell: ({ row }) => (
+                    <Cell>
+                        <PrimaryCellContent>
+                            {row.original.id}
+                        </PrimaryCellContent>
+                        <DateCellContent>
+                            {row.original.createdAt}
+                        </DateCellContent>
+                    </Cell>
+                )
             },
-          ] as ColumnDef<ExtendedHome, unknown>[])
-        : []),
-      {
-        accessorKey: "unit",
-        header: ColumnHeader("Unit"),
-        cell: ({ row }) => (
-          <Cell link="/community/unit/slug" slug={row.original?.slug}>
-            <PrimaryCellContent>
-              {row.original.lot}
-              {"/"}
-              {row.original.block}
-            </PrimaryCellContent>
-            <SecondaryCellContent>
-              {row.original?.modelName}
-            </SecondaryCellContent>
-          </Cell>
-        ),
-      },
-      // {
-      //   accessorKey: "model",
-      //   cell: ({ row }) => (
-      //     <Cell>
-      //       <SecondaryCellContent>
-      //         {row.original?.modelName}
-      //       </SecondaryCellContent>
-      //     </Cell>
-      //   ),
-      //   header: ColumnHeader("Model No"),
-      // },
-      // {
-      //   accessorKey: "lot",
-      //   header: ColumnHeader("Lot/Block"),
-      //   cell: ({ row }) => (
-      //     <Cell>
-      //       <PrimaryCellContent>
-      //         {row.original.lot}
-      //         {"/"}
-      //         {row.original.block}
-      //       </PrimaryCellContent>
-      //     </Cell>
-      //   ),
-      // },
+            ...(!projectView
+                ? ([
+                      {
+                          header: ColumnHeader("Project"),
+                          id: "title",
+                          cell: ({ row }) => (
+                              <Cell
+                                  link="/community/project/slug"
+                                  slug={row.original.project?.slug}
+                              >
+                                  <PrimaryCellContent>
+                                      {row.original?.project?.title}
+                                  </PrimaryCellContent>
 
-      {
-        accessorKey: "status",
-        header: ColumnHeader("Status"),
-        cell: ({ row }) => (
-          <Cell>
-            <HomeStatus home={row.original} />
-          </Cell>
-        ),
-      },
-      {
-        accessorKey: "invoice",
-        header: ColumnHeader("Invoice"),
-        cell: ({ row }) => <HomeInvoiceColumn home={row.original} />,
-      },
-      ..._FilterColumn(
-        "_status",
-        "_q",
-        "_builderId",
-        "_projectId"
-        // "_installation",
-        // "_production"
-      ),
-      {
-        accessorKey: "actions",
-        header: ColumnHeader(""),
-        size: 15,
-        maxSize: 15,
-        enableSorting: false,
-        cell: ({ row }) => (
-          <div className="">
-            <EditRowAction
-              onClick={(e) => {
-                openModal("editInvoice", row.original);
-              }}
+                                  <SecondaryCellContent>
+                                      {row.original?.project?.builder?.name}
+                                  </SecondaryCellContent>
+                              </Cell>
+                          )
+                      }
+                  ] as ColumnDef<ExtendedHome, unknown>[])
+                : []),
+            {
+                accessorKey: "unit",
+                header: ColumnHeader("Unit"),
+                cell: ({ row }) => (
+                    <Cell link="/community/unit/slug" slug={row.original?.slug}>
+                        <PrimaryCellContent>
+                            {row.original.lot}
+                            {"/"}
+                            {row.original.block}
+                        </PrimaryCellContent>
+                        <SecondaryCellContent>
+                            {row.original?.modelName}
+                        </SecondaryCellContent>
+                    </Cell>
+                )
+            },
+            // {
+            //   accessorKey: "model",
+            //   cell: ({ row }) => (
+            //     <Cell>
+            //       <SecondaryCellContent>
+            //         {row.original?.modelName}
+            //       </SecondaryCellContent>
+            //     </Cell>
+            //   ),
+            //   header: ColumnHeader("Model No"),
+            // },
+            // {
+            //   accessorKey: "lot",
+            //   header: ColumnHeader("Lot/Block"),
+            //   cell: ({ row }) => (
+            //     <Cell>
+            //       <PrimaryCellContent>
+            //         {row.original.lot}
+            //         {"/"}
+            //         {row.original.block}
+            //       </PrimaryCellContent>
+            //     </Cell>
+            //   ),
+            // },
+
+            {
+                accessorKey: "status",
+                header: ColumnHeader("Status"),
+                cell: ({ row }) => (
+                    <Cell>
+                        <HomeStatus home={row.original} />
+                    </Cell>
+                )
+            },
+            {
+                accessorKey: "invoice",
+                header: ColumnHeader("Invoice"),
+                cell: ({ row }) => <HomeInvoiceColumn home={row.original} />
+            },
+            ..._FilterColumn(
+                "_status",
+                "_q",
+                "_builderId",
+                "_projectId"
+                // "_installation",
+                // "_production"
+            ),
+            {
+                accessorKey: "actions",
+                header: ColumnHeader(""),
+                size: 15,
+                maxSize: 15,
+                enableSorting: false,
+                cell: ({ row }) => (
+                    <div className="">
+                        <EditRowAction
+                            onClick={e => {
+                                openModal("editInvoice", row.original);
+                            }}
+                        />
+                    </div>
+                )
+            }
+        ], //.filter(Boolean) as any,
+        [data, isPending]
+    );
+    return (
+        <>
+            <HomePrinter />
+            <DataTable2
+                searchParams={searchParams}
+                columns={columns}
+                pageInfo={pageInfo}
+                data={data}
+                SelectionAction={HomesSelectionAction}
+                filterableColumns={[
+                    BuilderFilter,
+                    ProjectsFilter,
+                    {
+                        id: "_production",
+                        title: "Production",
+                        single: true,
+                        options: [
+                            labelValue("Completed", "completed"),
+                            labelValue("Not In Production", "idle"),
+                            labelValue("Started", "started"),
+                            labelValue("Queued", "queued")
+                        ]
+                    },
+                    {
+                        id: "_installation",
+                        title: "Installation",
+                        single: true,
+                        options: [
+                            labelValue("Submitted", "submitted"),
+                            labelValue("No Submission", "no-submission")
+                        ]
+                    }
+                ]}
+                searchableColumns={[
+                    {
+                        id: "_q" as any,
+                        title: projectView
+                            ? "project Name,model,lot/block"
+                            : "model, lot/block"
+                    }
+                ]}
+
+                //  deleteRowsAction={() => void deleteSelectedRows()}
             />
-          </div>
-        ),
-      },
-    ], //.filter(Boolean) as any,
-    [data, isPending]
-  );
-  return (
-    <>
-      <HomePrinter />
-      <DataTable2
-        searchParams={searchParams}
-        columns={columns}
-        pageInfo={pageInfo}
-        data={data}
-        SelectionAction={HomesSelectionAction}
-        filterableColumns={[
-          BuilderFilter,
-          ProjectsFilter,
-          {
-            id: "_production",
-            title: "Production",
-            single: true,
-            options: [
-              labelValue("Completed", "completed"),
-              labelValue("Not In Production", "idle"),
-              labelValue("Started", "started"),
-              labelValue("Queued", "queued"),
-            ],
-          },
-          {
-            id: "_installation",
-            title: "Installation",
-            single: true,
-            options: [
-              labelValue("Submitted", "submitted"),
-              labelValue("No Submission", "no-submission"),
-            ],
-          },
-        ]}
-        searchableColumns={[
-          {
-            id: "_q" as any,
-            title: projectView
-              ? "project Name,model,lot/block"
-              : "model, lot/block",
-          },
-        ]}
-
-        //  deleteRowsAction={() => void deleteSelectedRows()}
-      />
-    </>
-  );
+        </>
+    );
 }
--- lib/community/community-utils.ts
+++ lib/community/community-utils.ts
@@ -3,67 +3,68 @@ import { getBadgeColor } from "../status-badge";
 import { Builders, Homes, Projects } from "@prisma/client";
 
 export function getHomeProductionStatus(home: ExtendedHome) {
-  const prod = home?.tasks?.filter((t) => t.produceable);
-  const produceables = prod?.length;
-  const produced = prod?.filter((p) => p.producedAt).length;
-  const pending = produceables - produced;
-  let productionStatus = "Idle";
-  const sent = prod?.filter((p) => p.sentToProductionAt)?.length;
+    const prod = home?.tasks?.filter(t => t.produceable);
+    const produceables = prod?.length;
+    const produced = prod?.filter(p => p.producedAt).length;
+    const pending = produceables - produced;
+    let productionStatus = "Idle";
+    const sent = prod?.filter(p => p.sentToProductionAt)?.length;
 
-  if (sent > 0) productionStatus = "Queued";
-  if (produced > 0) {
-    productionStatus = "Started";
-    if (produced == produceables) productionStatus = "Completed";
-  }
+    if (sent > 0) productionStatus = "Queued";
+    if (produced > 0) {
+        productionStatus = "Started";
+        if (produced == produceables) productionStatus = "Completed";
+    }
 
-  return {
-    produceables,
-    produced,
-    pendingProduction: pending,
-    productionStatus,
-    badgeColor: getBadgeColor(productionStatus),
-  };
+    return {
+        produceables,
+        produced,
+        pendingProduction: pending,
+        productionStatus,
+        badgeColor: getBadgeColor(productionStatus)
+    };
 }
 export function homeSearchMeta(
-  home: Homes,
-  project: Projects | undefined = undefined,
-  builder: Builders | undefined = undefined
+    home: Homes,
+    project: Projects | undefined = undefined,
+    builder: Builders | undefined = undefined
 ) {
-  const search: any[] = [];
-  const { modelName, lot, block } = home;
-  if (lot) search.push(`lot${lot} l${lot}`);
-  if (block) search.push(`blk${block} b${block}`);
-  if (lot && block) search.push(`${lot}/${block}`);
-  return search.join(" ");
+    const search: any[] = [];
+    const { modelName, lot, block } = home;
+    if (lot) search.push(`lot${lot} l${lot}`);
+    if (block) search.push(`blk${block} b${block}`);
+    if (lot && block) search.push(`${lot}/${block}`);
+    return search.join(" ");
 }
 export function hasConflictingDateRanges(dateRanges) {
-  dateRanges.sort((a, b) => a.startDate - b.startDate); // Sort date ranges by start date
+    dateRanges.sort((a, b) => a.startDate - b.startDate); // Sort date ranges by start date
 
-  for (let i = 0; i < dateRanges.length - 1; i++) {
-    const currentRange = dateRanges[i];
-    const nextRange = dateRanges[i + 1];
+    for (let i = 0; i < dateRanges.length - 1; i++) {
+        const currentRange = dateRanges[i];
+        const nextRange = dateRanges[i + 1];
 
-    if (currentRange.endDate >= nextRange.startDate) {
-      // Conflicting date ranges found
-      return true;
+        if (currentRange.endDate >= nextRange.startDate) {
+            // Conflicting date ranges found
+            return true;
+        }
     }
-  }
-  // No conflicting date ranges found
-  return false;
+    // No conflicting date ranges found
+    return false;
 }
 export function calculateHomeInvoice(home: ExtendedHome) {
-  const data = {
-    paid: 0,
-    due: 0,
-    chargeBack: 0,
-  };
-  home.tasks?.map((task) => {
-    const due = task.amountDue || 0;
-    const paid = task.amountPaid || 0;
-    if (paid > 0) {
-      data.due += due;
-      data.paid += paid;
-    } else data.chargeBack += paid;
-  });
-  return data;
+    const data = {
+        paid: 0,
+        due: 0,
+        chargeBack: 0
+    };
+    if (home.modelName == "5122 RH") console.log(home.tasks);
+    home.tasks?.map(task => {
+        const due = task.amountDue || 0;
+        const paid = task.amountPaid || 0;
+        if (paid => 0) {
+            data.due += due;
+            data.paid += paid;
+        } else data.chargeBack += paid;
+    });
+    return data;
 }
