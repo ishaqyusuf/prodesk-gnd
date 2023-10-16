510bea2f9267398dc78dd094d9fcdd8fca8b483b
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-10-16 21:09:38 +0100

    :tada:

--- app/_actions/community-production/get-productions.ts
+++ app/_actions/community-production/get-productions.ts
@@ -4,6 +4,7 @@ import { prisma } from "@/db";
 import { BaseQuery } from "@/types/action";
 import { getPageInfo, queryFilter } from "../action-utils";
 import { Prisma } from "@prisma/client";
+import { whereQuery } from "@/lib/db-utils";
 
 export interface ProductionsQueryParams extends BaseQuery {
     _projectId?;
@@ -29,6 +30,17 @@ function whereProductionQuery(query: ProductionsQueryParams) {
     const q = {
         contains: query._q || undefined
     };
+    const builder = whereQuery<Prisma.HomeTasksWhereInput>(query);
+    builder.raw({
+        produceable: true
+    });
+    builder.search({
+        home: {
+            OR: [{ modelName: builder.q }, { search: builder.q }]
+        }
+    });
+    builder.orWhere("projectId", +query._projectId);
+    return builder.get();
     const where: Prisma.HomeTasksWhereInput = {
         produceable: true,
         OR: query?._q
--- lib/db-utils.ts
+++ lib/db-utils.ts
@@ -1,77 +1,77 @@
 import { getPageInfo, queryFilter } from "@/app/_actions/action-utils";
 
 export function searchQuery<T>(query, ...columns: (keyof T)[]) {
-  if (!query._q) return {};
-  const q = {
-    contains: query._q || undefined,
-  };
-  const OR: any = [];
-  columns.map((c) => {
-    OR.push({
-      [c]: q,
+    if (!query._q) return {};
+    const q = {
+        contains: query._q || undefined
+    };
+    const OR: any = [];
+    columns.map(c => {
+        OR.push({
+            [c]: q
+        });
     });
-  });
-  return {
-    OR,
-  };
+    return {
+        OR
+    };
 }
 export async function queryBuilder<T>(query, table) {
-  const where = whereQuery<T>(query);
-  const queryFilters = await queryFilter(query);
-  return {
-    ...where,
-    getWhere: where.get,
-    queryFilters,
-    async response(data) {
-      const pageInfo = await getPageInfo(query, where.get(), table);
-      return {
-        pageInfo,
-        data,
-      };
-    },
-  };
+    const where = whereQuery<T>(query);
+    const queryFilters = await queryFilter(query);
+    return {
+        ...where,
+        getWhere: where.get,
+        queryFilters,
+        async response(data) {
+            const pageInfo = await getPageInfo(query, where.get(), table);
+            return {
+                pageInfo,
+                data
+            };
+        }
+    };
 }
 export function whereQuery<T>(query) {
-  let where: any = {} as any;
-  const q = {
-    contains: query._q || undefined,
-  };
-  return {
-    where,
-    get: () => where as any,
-    register(column: keyof T, value: any) {
-      where[column] = value;
-    },
-    orWhere(column: keyof T, value: any) {
-      this.register(column, value || undefined);
-    },
-    searchRelationQuery: <T1>(...columns: (keyof T)[]) => {
-      Object.entries(searchQuery<T>(query, ...columns)).map(
-        ([k, v]) => (where[k] = v)
-      );
-    },
-    q,
-    raw(rq: T) {
-      where = {
-        ...where,
-        ...rq,
-      };
-      // Object.entries(rq as any).map(([k, v]) => (where[k] = v));
-    },
-    search(_search: T) {
-      if (q.contains) {
-        if (!where.OR) where.OR = [];
-        Object.entries(_search as any).map(([k, v]) => {
-          where.OR.push({
-            [k]: v,
-          });
-        });
-      }
-    },
-    searchQuery: (...columns: (keyof T)[]) => {
-      Object.entries(searchQuery<T>(query, ...columns)).map(
-        ([k, v]) => (where[k] = v)
-      );
-    },
-  };
+    let where: any = {} as any;
+    const q = {
+        contains: query._q || undefined
+    };
+    return {
+        where,
+        get: () => where as any,
+        register(column: keyof T, value: any) {
+            where[column] = value;
+        },
+        orWhere(column: keyof T, value: any) {
+            if (value) this.register(column, value || undefined);
+        },
+        searchRelationQuery: <T1>(...columns: (keyof T)[]) => {
+            Object.entries(searchQuery<T>(query, ...columns)).map(
+                ([k, v]) => (where[k] = v)
+            );
+        },
+        q,
+        raw(rq: T) {
+            where = {
+                ...where,
+                ...rq
+            };
+            // Object.entries(rq as any).map(([k, v]) => (where[k] = v));
+        },
+        search(_search: T) {
+            if (q.contains) {
+                if (!where.OR) where.OR = [];
+                Object.entries(_search as any).map(([k, v]) => {
+                    where.OR.push({
+                        [k]: v
+                    });
+                });
+            }
+        },
+        searchQuery: (...columns: (keyof T)[]) => {
+            Object.entries(searchQuery<T>(query, ...columns)).map(
+                ([k, v]) => (where[k] = v)
+            );
+        }
+    };
 }
