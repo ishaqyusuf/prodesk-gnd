132935cd138669d88de1234dceecf9b8890b3663
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-10-16 22:04:47 +0100

    :tada:

--- components/shells/estimates-table-shell.tsx
+++ components/shells/estimates-table-shell.tsx
@@ -5,15 +5,16 @@ import { TableShellProps } from "@/types/data-table";
 import { ColumnDef } from "@tanstack/react-table";
 import { useMemo, useState, useTransition } from "react";
 import {
-  CheckColumn,
-  ColumnHeader,
-  _FilterColumn,
+    CheckColumn,
+    ColumnHeader,
+    _FilterColumn
 } from "../columns/base-columns";
 import { DataTable } from "../data-table/data-table";
 import {
-  OrderCustomerCell,
-  OrderIdCell,
-  OrderInvoiceCell,
+    OrderCustomerCell,
+    OrderIdCell,
+    OrderInvoiceCell,
+    OrderMemoCell
 } from "../columns/sales-columns";
 import { ISalesOrder } from "@/types/sales";
 import { OrderRowAction } from "../actions/order-actions";
@@ -22,66 +23,79 @@ import { SalesSelectionAction } from "../sales/sales-selection-action";
 import { SalesCustomerFilter } from "../filters/sales-customer-filter";
 
 export default function EstimatesTableShell<T>({
-  data,
-  pageInfo,
-  searchParams,
+    data,
+    pageInfo,
+    searchParams
 }: TableShellProps<ISalesOrder>) {
-  const [isPending, startTransition] = useTransition();
+    const [isPending, startTransition] = useTransition();
 
-  const [selectedRowIds, setSelectedRowIds] = useState<number[]>([]);
-  const columns = useMemo<ColumnDef<ISalesOrder, unknown>[]>(
-    () => [
-      CheckColumn({ selectedRowIds, setSelectedRowIds, data }),
+    const [selectedRowIds, setSelectedRowIds] = useState<number[]>([]);
+    const columns = useMemo<ColumnDef<ISalesOrder, unknown>[]>(
+        () => [
+            CheckColumn({ selectedRowIds, setSelectedRowIds, data }),
 
-      {
-        accessorKey: "orderId",
-        cell: ({ row }) => OrderIdCell(row.original, "/sales/estimate/slug"),
-        header: ColumnHeader("Estimate #"),
-      },
-      {
-        accessorKey: "customer",
-        header: ColumnHeader("Customer"),
-        cell: ({ row }) =>
-          OrderCustomerCell(row.original.customer, "/sales/customer/slug"),
-      },
-      {
-        accessorKey: "invoice",
-        header: ColumnHeader("Total"),
-        cell: ({ row }) => <OrderInvoiceCell order={row.original} isEstimate />,
-      },
+            {
+                accessorKey: "orderId",
+                cell: ({ row }) =>
+                    OrderIdCell(row.original, "/sales/estimate/slug"),
+                header: ColumnHeader("Estimate #")
+            },
+            {
+                accessorKey: "customer",
+                header: ColumnHeader("Customer"),
+                cell: ({ row }) =>
+                    OrderCustomerCell(
+                        row.original.customer,
+                        "/sales/customer/slug"
+                    )
+            },
+            {
+                accessorKey: "memo",
+                header: ColumnHeader("Address"),
+                cell: ({ row }) => OrderMemoCell(row.original.shippingAddress)
+            },
+            {
+                accessorKey: "invoice",
+                header: ColumnHeader("Total"),
+                cell: ({ row }) => (
+                    <OrderInvoiceCell order={row.original} isEstimate />
+                )
+            },
 
-      ..._FilterColumn("_q", "_status", "_date", "_customerId"),
-      {
-        accessorKey: "actions",
-        header: ColumnHeader(""),
-        size: 15,
-        maxSize: 15,
-        enableSorting: false,
-        cell: ({ row }) => <OrderRowAction estimate row={row.original} />,
-      },
-    ],
-    [data, isPending]
-  );
-  return (
-    <DataTable2
-      searchParams={searchParams}
-      columns={columns}
-      pageInfo={pageInfo}
-      data={data}
-      SelectionAction={SalesSelectionAction}
-      filterableColumns={[SalesCustomerFilter]}
-      searchableColumns={[
-        {
-          id: "_q" as any,
-          title: "estimate id, customer...",
-        },
-      ]}
-      dateFilterColumns={[
-        {
-          id: "_date" as any,
-          title: "Date",
-        },
-      ]}
-    />
-  );
+            ..._FilterColumn("_q", "_status", "_date", "_customerId"),
+            {
+                accessorKey: "actions",
+                header: ColumnHeader(""),
+                size: 15,
+                maxSize: 15,
+                enableSorting: false,
+                cell: ({ row }) => (
+                    <OrderRowAction estimate row={row.original} />
+                )
+            }
+        ],
+        [data, isPending]
+    );
+    return (
+        <DataTable2
+            searchParams={searchParams}
+            columns={columns}
+            pageInfo={pageInfo}
+            data={data}
+            SelectionAction={SalesSelectionAction}
+            filterableColumns={[SalesCustomerFilter]}
+            searchableColumns={[
+                {
+                    id: "_q" as any,
+                    title: "estimate id, customer..."
+                }
+            ]}
+            dateFilterColumns={[
+                {
+                    id: "_date" as any,
+                    title: "Date"
+                }
+            ]}
+        />
+    );
 }
--- components/shells/orders-table-shell.tsx
+++ components/shells/orders-table-shell.tsx
@@ -5,19 +5,19 @@ import { TableShellProps } from "@/types/data-table";
 import { ColumnDef } from "@tanstack/react-table";
 import { useEffect, useMemo, useState, useTransition } from "react";
 import {
-  CheckColumn,
-  ColumnHeader,
-  _FilterColumn,
+    CheckColumn,
+    ColumnHeader,
+    _FilterColumn
 } from "../columns/base-columns";
 import { DataTable } from "../data-table/data-table";
 import {
-  OrderCustomerCell,
-  OrderIdCell,
-  OrderInvoiceCell,
-  OrderMemoCell,
-  OrderPriorityFlagCell,
-  OrderProductionStatusCell,
-  OrderStatus,
+    OrderCustomerCell,
+    OrderIdCell,
+    OrderInvoiceCell,
+    OrderMemoCell,
+    OrderPriorityFlagCell,
+    OrderProductionStatusCell,
+    OrderStatus
 } from "../columns/sales-columns";
 import { ISalesOrder } from "@/types/sales";
 import { OrderRowAction } from "../actions/order-actions";
@@ -27,107 +27,120 @@ import { SalesSelectionAction } from "../sales/sales-selection-action";
 import { SalesCustomerFilter } from "../filters/sales-customer-filter";
 
 export default function OrdersTableShell<T>({
-  data,
-  pageInfo,
-  searchParams,
+    data,
+    pageInfo,
+    searchParams
 }: TableShellProps<ISalesOrder>) {
-  const [isPending, startTransition] = useTransition();
+    const [isPending, startTransition] = useTransition();
 
-  const [selectedRowIds, setSelectedRowIds] = useState<number[]>([]);
-  const columns = useMemo<ColumnDef<ISalesOrder, unknown>[]>(
-    () => [
-      CheckColumn({ selectedRowIds, setSelectedRowIds, data }),
-      {
-        maxSize: 10,
-        id: "flags",
-        cell: ({ row }) => OrderPriorityFlagCell(row.original, true),
-      },
-      {
-        accessorKey: "orderId",
-        cell: ({ row }) => OrderIdCell(row.original, "/sales/order/slug"),
-        header: ColumnHeader("Order"),
-      },
-      {
-        accessorKey: "customer",
-        header: ColumnHeader("Customer"),
-        cell: ({ row }) =>
-          OrderCustomerCell(row.original.customer, "/sales/customer/slug"),
-      },
-      {
-        accessorKey: "memo",
-        header: ColumnHeader("Memo"),
-        cell: ({ row }) => OrderMemoCell(row.original.shippingAddress),
-      },
-      {
-        accessorKey: "invoice",
-        header: ColumnHeader("Total/Due"),
-        cell: ({ row }) => <OrderInvoiceCell order={row.original} />,
-      },
-      {
-        accessorKey: "production",
-        header: ColumnHeader("Production"),
-        cell: ({ row }) => OrderProductionStatusCell(row.original),
-      },
-      {
-        accessorKey: "status",
-        header: ColumnHeader("Status"),
-        cell: ({ row }) => OrderStatus(row.original),
-      },
-      ..._FilterColumn("_status", "_q", "_payment", "_customerId", "_date"),
-      {
-        accessorKey: "actions",
-        header: ColumnHeader(""),
-        size: 15,
-        maxSize: 15,
-        enableSorting: false,
-        cell: ({ row }) => <OrderRowAction row={row.original} />,
-      },
-    ],
-    [data, isPending]
-  );
-  return (
-    <DataTable2
-      searchParams={searchParams}
-      columns={columns}
-      pageInfo={pageInfo}
-      data={data}
-      SelectionAction={SalesSelectionAction}
-      filterableColumns={[
-        {
-          id: "status",
-          title: "Status",
-          single: true,
-          options: [
-            { label: "Production Started", value: "Started" },
-            { label: "Production Assigned", value: "Queued" },
-            { label: "Production Completed", value: "Completed" },
-            { label: "Production Not Assigned", value: "Unassigned" },
-          ],
-        },
-        {
-          id: "_payment" as any,
-          title: "Invoice",
-          single: true,
-          options: [
-            { label: "Paid", value: "Paid" },
-            // { label: "Part Paid", value: "Part" },
-            { label: "Pending", value: "Pending" },
-          ],
-        },
-        SalesCustomerFilter,
-      ]}
-      searchableColumns={[
-        {
-          id: "_q" as any,
-          title: "orderId, customer",
-        },
-      ]}
-      dateFilterColumns={[
-        {
-          id: "_date" as any,
-          title: "Date",
-        },
-      ]}
-    />
-  );
+    const [selectedRowIds, setSelectedRowIds] = useState<number[]>([]);
+    const columns = useMemo<ColumnDef<ISalesOrder, unknown>[]>(
+        () => [
+            CheckColumn({ selectedRowIds, setSelectedRowIds, data }),
+            {
+                maxSize: 10,
+                id: "flags",
+                cell: ({ row }) => OrderPriorityFlagCell(row.original, true)
+            },
+            {
+                accessorKey: "orderId",
+                cell: ({ row }) =>
+                    OrderIdCell(row.original, "/sales/order/slug"),
+                header: ColumnHeader("Order")
+            },
+            {
+                accessorKey: "customer",
+                header: ColumnHeader("Customer"),
+                cell: ({ row }) =>
+                    OrderCustomerCell(
+                        row.original.customer,
+                        "/sales/customer/slug"
+                    )
+            },
+            {
+                accessorKey: "memo",
+                header: ColumnHeader("Address"),
+                cell: ({ row }) => OrderMemoCell(row.original.shippingAddress)
+            },
+            {
+                accessorKey: "invoice",
+                header: ColumnHeader("Total/Due"),
+                cell: ({ row }) => <OrderInvoiceCell order={row.original} />
+            },
+            {
+                accessorKey: "production",
+                header: ColumnHeader("Production"),
+                cell: ({ row }) => OrderProductionStatusCell(row.original)
+            },
+            {
+                accessorKey: "status",
+                header: ColumnHeader("Status"),
+                cell: ({ row }) => OrderStatus(row.original)
+            },
+            ..._FilterColumn(
+                "_status",
+                "_q",
+                "_payment",
+                "_customerId",
+                "_date"
+            ),
+            {
+                accessorKey: "actions",
+                header: ColumnHeader(""),
+                size: 15,
+                maxSize: 15,
+                enableSorting: false,
+                cell: ({ row }) => <OrderRowAction row={row.original} />
+            }
+        ],
+        [data, isPending]
+    );
+    return (
+        <DataTable2
+            searchParams={searchParams}
+            columns={columns}
+            pageInfo={pageInfo}
+            data={data}
+            SelectionAction={SalesSelectionAction}
+            filterableColumns={[
+                {
+                    id: "status",
+                    title: "Status",
+                    single: true,
+                    options: [
+                        { label: "Production Started", value: "Started" },
+                        { label: "Production Assigned", value: "Queued" },
+                        { label: "Production Completed", value: "Completed" },
+                        {
+                            label: "Production Not Assigned",
+                            value: "Unassigned"
+                        }
+                    ]
+                },
+                {
+                    id: "_payment" as any,
+                    title: "Invoice",
+                    single: true,
+                    options: [
+                        { label: "Paid", value: "Paid" },
+                        // { label: "Part Paid", value: "Part" },
+                        { label: "Pending", value: "Pending" }
+                    ]
+                },
+                SalesCustomerFilter
+            ]}
+            searchableColumns={[
+                {
+                    id: "_q" as any,
+                    title: "orderId, customer"
+                }
+            ]}
+            dateFilterColumns={[
+                {
+                    id: "_date" as any,
+                    title: "Date"
+                }
+            ]}
+        />
+    );
 }
