3cc5174bbe6ef47d3ea1a3f18ca91e05c3c54c79
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-10-25 14:49:58 +0100

    :tada:

--- app/_actions/hrm-jobs/make-payment.ts
+++ app/_actions/hrm-jobs/make-payment.ts
@@ -7,87 +7,88 @@ import { userId } from "../utils";
 import { _notifyProdStarted, _notifyWorkerPaymentPaid } from "../notifications";
 
 export async function getPayableUsers(userId) {
-  const users = await prisma.users.findMany({
-    where: {},
-    include: {
-      employeeProfile: true,
-      jobs: {
-        where: {
-          paymentId: {
-            equals: null,
-          },
-        },
-      },
-    },
-  });
-  const payables = users
-    .filter((user) => user.jobs.length > 0)
+    const users = await prisma.users.findMany({
+        where: {},
+        include: {
+            employeeProfile: true,
+            jobs: {
+                where: {
+                    deletedAt: null,
+                    paymentId: {
+                        equals: null
+                    }
+                }
+            }
+        }
+    });
+    const payables = users
+        .filter(user => user.jobs.length > 0)
 
-    .map((user) => {
-      const vjobs: Jobs[] = user.jobs.filter((j) => !j.paymentId);
+        .map(user => {
+            const vjobs: Jobs[] = user.jobs.filter(j => !j.paymentId);
 
-      let total = !vjobs ? 0 : +(sum(vjobs, "amount") || 0);
-      //   if(!total)return null;
-      if (user.employeeProfile) {
-        console.log("EMPLOYEE PROFILE");
-        console.log(user.employeeProfile);
-      }
-      let chargePercentage = user.employeeProfile?.discount || 0;
-      let charge = 0;
-      if (chargePercentage > 0)
-        charge = (total || 0) * (chargePercentage / 100);
-      return {
-        jobIds: vjobs.map((v) => v.id),
-        id: user.id,
-        pid: user.employeeProfileId,
-        name: user.name,
-        subTotal: total,
-        charge,
-        chargePercentage,
-        total: total - charge,
-        profile: user.employeeProfile,
-      };
-    })
-    .filter((u) => u != null);
+            let total = !vjobs ? 0 : +(sum(vjobs, "amount") || 0);
+            //   if(!total)return null;
+            if (user.employeeProfile) {
+                console.log("EMPLOYEE PROFILE");
+                console.log(user.employeeProfile);
+            }
+            let chargePercentage = user.employeeProfile?.discount || 0;
+            let charge = 0;
+            if (chargePercentage > 0)
+                charge = (total || 0) * (chargePercentage / 100);
+            return {
+                jobIds: vjobs.map(v => v.id),
+                id: user.id,
+                pid: user.employeeProfileId,
+                name: user.name,
+                subTotal: total,
+                charge,
+                chargePercentage,
+                total: total - charge,
+                profile: user.employeeProfile
+            };
+        })
+        .filter(u => u != null);
 
-  const jobs = !userId
-    ? []
-    : await prisma.jobs.findMany({
-        where: {
-          paymentId: null,
-          userId: Number(userId),
-        },
-      });
-  return {
-    payables,
-    jobs: {
-      data: jobs,
-      pageInfo: {},
-    },
-  };
+    const jobs = !userId
+        ? []
+        : await prisma.jobs.findMany({
+              where: {
+                  paymentId: null,
+                  userId: Number(userId)
+              }
+          });
+    return {
+        payables,
+        jobs: {
+            data: jobs,
+            pageInfo: {}
+        }
+    };
 }
 interface Props {
-  jobIds: number[];
-  payment: Partial<JobPayments>;
+    jobIds: number[];
+    payment: Partial<JobPayments>;
 }
 export async function makePayment({ payment, jobIds }: Props) {
-  payment.paidBy = await userId();
+    payment.paidBy = await userId();
 
-  const p = await prisma.jobPayments.create({
-    data: transformData(payment) as any,
-  });
-  const jobs = await prisma.jobs.updateMany({
-    where: {
-      id: {
-        in: jobIds,
-      },
-    },
-    data: {
-      status: "Paid",
-      paymentId: p.id,
-      statusDate: new Date(),
-    },
-  });
-  //   jobs.
-  await _notifyWorkerPaymentPaid(p, jobs.count);
+    const p = await prisma.jobPayments.create({
+        data: transformData(payment) as any
+    });
+    const jobs = await prisma.jobs.updateMany({
+        where: {
+            id: {
+                in: jobIds
+            }
+        },
+        data: {
+            status: "Paid",
+            paymentId: p.id,
+            statusDate: new Date()
+        }
+    });
+    //   jobs.
+    await _notifyWorkerPaymentPaid(p, jobs.count);
 }
--- app/_actions/hrm.ts
+++ app/_actions/hrm.ts
@@ -3,31 +3,32 @@
 import { prisma } from "@/db";
 
 export async function getProductionUsersAction() {
-  const users = await prisma.users.findMany({
-    include: {
-      _count: {
-        select: {
-          reppedProductions: {
-            where: {
-              // prodDueDate:{}
-              prodStatus: {
-                notIn: ["Completed"],
-              },
-            },
-          },
+    const users = await prisma.users.findMany({
+        include: {
+            _count: {
+                select: {
+                    reppedProductions: {
+                        where: {
+                            // prodDueDate:{}
+                            prodStatus: {
+                                notIn: ["Completed"]
+                            }
+                        }
+                    }
+                }
+            }
         },
-      },
-    },
-    where: {
-      roles: {
-        some: {
-          role: {
-            name: "Production",
-          },
-        },
-      },
-    },
-  });
-  console.log(users);
-  return users;
+        where: {
+            deletedAt: null,
+            roles: {
+                some: {
+                    role: {
+                        name: "Production"
+                    }
+                }
+            }
+        }
+    });
+    console.log(users);
+    return users;
 }
