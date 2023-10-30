322c967f252cf173b8a91b6a940de22c16a4e10b
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-10-30 15:37:19 +0100

    :tada:

--- components/forms/sales-inbound-order-form/inbound-form.tsx
+++ components/forms/sales-inbound-order-form/inbound-form.tsx
@@ -1,22 +1,21 @@
 "use client";
 
-import InboundModal from "@/components/modals/inbound-modal";
 import PageHeader from "@/components/page-header";
 import InboundFormTableShell from "@/components/shells/inbound-form-table-shell";
 import { IInboundOrder } from "@/types/sales-inbound";
 
 interface Props {
-  list: any;
-  form: IInboundOrder;
-  suppliers: string[];
+    list: any;
+    form: IInboundOrder;
+    suppliers: string[];
 }
 export default function InboundForm({ form, list, suppliers }: Props) {
-  async function create() {}
-  return (
-    <>
-      <PageHeader title="New Inbound" />
-      <InboundFormTableShell {...list} suppliers={suppliers} />
-      <InboundModal />
-    </>
-  );
+    async function create() {}
+    return (
+        <>
+            <PageHeader title="New Inbound" />
+            <InboundFormTableShell {...list} suppliers={suppliers} />
+        </>
+    );
 }
+
