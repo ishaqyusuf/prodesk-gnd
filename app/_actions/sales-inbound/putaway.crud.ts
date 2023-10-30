06e34ca82f51b02c522d9cfba332678c013a6c4c
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-10-30 15:34:09 +0100

    :tada:

--- app/_actions/sales-inbound/putaway.crud.ts
+++ app/_actions/sales-inbound/putaway.crud.ts
@@ -7,98 +7,44 @@ import { Prisma } from "@prisma/client";
 import { getPageInfo, queryFilter } from "../action-utils";
 
 interface PutawayQueryParams extends Omit<BaseQuery, "status"> {
-  status: "All" | "Pending" | "Stored" | "Pending Arrival";
+    status: "All" | "Pending" | "Stored" | "Pending Arrival";
 }
 export async function getPutwaysAction(query: PutawayQueryParams) {
-  // if (!query.status) query.status = "Arrived Warehouse";
-  const where = wherePutaway(query);
-  const items = await prisma.inboundOrderItems.findMany({
-    where,
-    ...(await queryFilter(query)),
-    include: {
-      InboundOrder: true,
-      salesOrderItems: {
+    // if (!query.status) query.status = "Arrived Warehouse";
+    const where = wherePutaway(query);
+    const items = await prisma.salesItemSupply.findMany({
+        where,
+        ...(await queryFilter(query)),
         include: {
-          salesOrder: {
-            select: {
-              id: true,
-              slug: true,
-              orderId: true,
-            },
-          },
-        },
-      },
-    },
-  });
-  const pageInfo = await getPageInfo(query, where, prisma.inboundOrderItems);
-  return {
-    pageInfo,
-    data: items as any,
-  };
+            // InboundOrder: true,
+            // salesOrderItems: {
+            //     include: {
+            //         salesOrder: {
+            //             select: {
+            //                 id: true,
+            //                 slug: true,
+            //                 orderId: true
+            //             }
+            //         }
+            //     }
+            // }
+        }
+    });
+    const pageInfo = await getPageInfo(query, where, prisma.salesItemSupply);
+    return {
+        pageInfo,
+        data: items as any
+    };
 }
 function wherePutaway(query: PutawayQueryParams) {
-  const queryBuilder = whereQuery<Prisma.InboundOrderItemsWhereInput>(query);
-  // queryBuilder.register("status", query.status);
-
-  queryBuilder.search({
-    salesOrderItems: {
-      OR: [
-        {
-          description: queryBuilder.q,
-        },
-        {
-          salesOrder: {
-            OR: [
-              {
-                orderId: queryBuilder.q,
-              },
-              {
-                customer: {
-                  name: queryBuilder.q,
-                },
-              },
-            ],
-          },
-        },
-      ],
-    },
-  });
-  queryBuilder.search({
-    InboundOrder: {
-      slug: queryBuilder.q,
-    },
-  });
-  switch (query.status) {
-    case "Pending":
-      queryBuilder.raw({
-        location: null,
-        status: "Arrived Warehouse",
-      });
-      break;
-    case "Pending Arrival":
-      queryBuilder.raw({
-        location: null,
-        status: {
-          not: "Arrived Warehouse",
-        },
-      });
-      break;
-    case "Stored":
-      queryBuilder.raw({
-        location: {
-          not: null,
-        },
-      });
-      break;
-  }
-  return queryBuilder.get();
+    return {};
 }
 export async function _updateInboundItemLocation(id, data) {
-  await prisma.inboundOrderItems.update({
-    where: { id },
-    data: {
-      ...data,
-      updatedAt: new Date(),
-    },
-  });
+    await prisma.salesItemSupply.update({
+        where: { id },
+        data: {
+            ...data,
+            updatedAt: new Date()
+        }
+    });
 }
