613e4edcfbeaa3f35550c12374102ea39b601c23
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-11-02 15:22:46 +0100

    :tada:

--- components/forms/sales-form/sales-invoice-table.tsx
+++ components/forms/sales-form/sales-invoice-table.tsx
@@ -1,9 +1,9 @@
 import {
-  Table,
-  TableBody,
-  TableHead,
-  TableHeader,
-  TableRow,
+    Table,
+    TableBody,
+    TableHead,
+    TableHeader,
+    TableRow
 } from "@/components/ui/table";
 import { useFieldArray } from "react-hook-form";
 import * as React from "react";
@@ -17,115 +17,121 @@ import { moreInvoiceLines } from "@/lib/sales/sales-invoice-form";
 import { SalesFormResponse } from "@/app/_actions/sales/sales-form";
 import SalesComponentModal from "@/components/modals/sales-component-modal";
 export default function SalesInvoiceTable({
-  form,
-  data,
+    form,
+    data
 }: {
-  form: ISalesOrderForm;
-  data: SalesFormResponse;
+    form: ISalesOrderForm;
+    data: SalesFormResponse;
 }) {
-  const { watch, control, register } = form;
-  const watchItems = watch("items");
-  const watchProfileEstimate = watch("meta.profileEstimate");
-  const profileChange = watch("meta.sales_percentage");
-  const { fields, replace } = useFieldArray({
-    control,
-    name: "items",
-  });
-  React.useEffect(() => {
-    startTransition(() => {});
-  }, [watchProfileEstimate, profileChange]);
-  const tableRef = React.useRef(null);
-  const [floatingFooter, setFloatingFooter] = React.useState(false);
-
-  const hideFooter = useLoader();
-  React.useEffect(() => {
-    const handleIntersection = (entries) => {
-      const [entry] = entries;
-      setFloatingFooter(entry.isIntersecting == false);
-    };
-    const observer = new IntersectionObserver(handleIntersection, {
-      // rootMargin: "0px 0px 0px 0px",
+    const { watch, control, register } = form;
+    const watchItems = watch("items");
+    const watchProfileEstimate = watch("meta.profileEstimate");
+    const profileChange = watch("meta.sales_percentage");
+    const { fields, replace } = useFieldArray({
+        control,
+        name: "items"
     });
+    React.useEffect(() => {
+        startTransition(() => {});
+    }, [watchProfileEstimate, profileChange]);
+    const tableRef = React.useRef(null);
+    const [floatingFooter, setFloatingFooter] = React.useState(false);
 
-    if (tableRef.current) {
-      observer.observe(tableRef.current);
-    }
+    const hideFooter = useLoader();
+    React.useEffect(() => {
+        const handleIntersection = entries => {
+            const [entry] = entries;
+            setFloatingFooter(entry.isIntersecting == false);
+        };
+        const observer = new IntersectionObserver(handleIntersection, {
+            // rootMargin: "0px 0px 0px 0px",
+        });
 
-    return () => {
-      if (tableRef.current) {
-        observer.unobserve(tableRef.current);
-      }
-    };
-  }, [hideFooter.isLoading]);
-  const [isPending, startTransition] = React.useTransition();
+        if (tableRef.current) {
+            observer.observe(tableRef.current);
+        }
 
-  return (
-    <div className="relative">
-      <Table className="">
-        <TableHeader>
-          <TableRow>
-            {/* <TableHead className="w-[100px]">Invoice</TableHead> */}
-            <TableHead className="w-[25px] px-1">#</TableHead>
-            <TableHead className="w-5 px-1">
-              <Layers className="h-3.5 w-3.5" />
-            </TableHead>
-            <TableHead className="px-1">Item</TableHead>
-            <TableHead className="w-20  px-1">Swing</TableHead>
-            <TableHead className="w-20 px-1">Supplier</TableHead>
-            <TableHead className="w-14 px-1 text-center">Qty</TableHead>
-            <TableHead className="w-20 px-1">Cost</TableHead>
-            {watchProfileEstimate ? (
-              <>
-                <TableHead className="w-8 px-1">Rate</TableHead>
-              </>
-            ) : (
-              <></>
-            )}
-            <TableHead className="w-8 px-1 text-right">Total</TableHead>
-            <TableHead className="w-8 px-1 text-center">Tax</TableHead>
-            <TableHead className="w-10 px-1"></TableHead>
-          </TableRow>
-        </TableHeader>
+        return () => {
+            if (tableRef.current) {
+                observer.unobserve(tableRef.current);
+            }
+        };
+    }, [hideFooter.isLoading]);
+    const [isPending, startTransition] = React.useTransition();
 
-        <TableBody>
-          {fields.map((field, i) => (
-            <SalesInvoiceTr
-              ctx={data.ctx}
-              rowIndex={i}
-              startTransition2={startTransition}
-              isPending={isPending}
-              field={field}
-              form={form}
-              key={field.id}
+    return (
+        <div className="relative">
+            <Table className="w-auto">
+                <TableHeader>
+                    <TableRow>
+                        {/* <TableHead className="w-[100px]">Invoice</TableHead> */}
+                        <TableHead className="w-[25px] px-1">#</TableHead>
+                        <TableHead className="w-5 px-1">
+                            <Layers className="h-3.5 w-3.5" />
+                        </TableHead>
+                        <TableHead className="px-1">Item</TableHead>
+                        <TableHead className="w-20  px-1">Swing</TableHead>
+                        <TableHead className="w-20 px-1">Supplier</TableHead>
+                        <TableHead className="w-14 px-1 text-center">
+                            Qty
+                        </TableHead>
+                        <TableHead className="w-20 px-1">Cost</TableHead>
+                        {watchProfileEstimate ? (
+                            <>
+                                <TableHead className="w-8 px-1">Rate</TableHead>
+                            </>
+                        ) : (
+                            <></>
+                        )}
+                        <TableHead className="w-8 px-1 text-right">
+                            Total
+                        </TableHead>
+                        <TableHead className="w-8 px-1 text-center">
+                            Tax
+                        </TableHead>
+                        <TableHead className="w-10 px-1"></TableHead>
+                    </TableRow>
+                </TableHeader>
+
+                <TableBody>
+                    {fields.map((field, i) => (
+                        <SalesInvoiceTr
+                            ctx={data.ctx}
+                            rowIndex={i}
+                            startTransition2={startTransition}
+                            isPending={isPending}
+                            field={field}
+                            form={form}
+                            key={field.id}
+                        />
+                    ))}
+                </TableBody>
+            </Table>
+            <div className="flex">
+                <Button
+                    className="w-full"
+                    onClick={() => {
+                        hideFooter.action(() => {
+                            replace(moreInvoiceLines(watchItems as any));
+                        });
+                    }}
+                    variant="ghost"
+                >
+                    More Lines
+                </Button>
+            </div>
+            {floatingFooter && <div className="h-[250px]"></div>}
+            <div id="lastLine" ref={tableRef} className=""></div>
+            <InvoiceTableFooter
+                ctx={data.ctx}
+                floatingFooter={floatingFooter}
+                form={form}
             />
-          ))}
-        </TableBody>
-      </Table>
-      <div className="flex">
-        <Button
-          className="w-full"
-          onClick={() => {
-            hideFooter.action(() => {
-              replace(moreInvoiceLines(watchItems as any));
-            });
-          }}
-          variant="ghost"
-        >
-          More Lines
-        </Button>
-      </div>
-      {floatingFooter && <div className="h-[250px]"></div>}
-      <div id="lastLine" ref={tableRef} className=""></div>
-      <InvoiceTableFooter
-        ctx={data.ctx}
-        floatingFooter={floatingFooter}
-        form={form}
-      />
-      <SalesComponentModal
-        startTransition2={startTransition}
-        form={form}
-        ctx={data.ctx}
-      />
-    </div>
-  );
+            <SalesComponentModal
+                startTransition2={startTransition}
+                form={form}
+                ctx={data.ctx}
+            />
+        </div>
+    );
 }
