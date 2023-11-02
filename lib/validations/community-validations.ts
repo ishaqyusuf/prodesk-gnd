20bf768ee55e65b8e6654969e7654f3b7d98035f
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-11-02 16:57:38 +0100

    :tada:

--- lib/validations/community-validations.ts
+++ lib/validations/community-validations.ts
@@ -1,31 +1,31 @@
 import { z } from "zod";
 
 export const projectSchema = z.object({
-  title: z.string(),
-  refNo: z.string().optional(),
-  builderId: z.number(),
-  address: z.string().optional(),
-  meta: z
-    .object({
-      supervisor: z
+    title: z.string(),
+    refNo: z.string().optional(),
+    builderId: z.number(),
+    address: z.string().optional(),
+    meta: z
         .object({
-          name: z.string().optional(),
-          email: z.string().optional(),
+            supervisor: z
+                .object({
+                    name: z.string().optional(),
+                    email: z.string().optional()
+                })
+                .optional()
         })
-        .optional(),
-    })
-    .optional(),
+        .optional()
 });
 export const homeSchema = z.object({
-  projectId: z.number(),
-  units: z
-    .object({
-      homeTemplateId: z.number(),
-      lot: z.string().optional(),
-      block: z.string().optional(),
-      createdAt: z.date().optional(),
-      homeKey: z.string().optional(),
-      meta: z.object({}),
-    })
-    .array(),
+    projectId: z.number(),
+    units: z
+        .object({
+            communityTemplateId: z.number(),
+            lot: z.string().optional(),
+            block: z.string().optional(),
+            createdAt: z.date().optional(),
+            homeKey: z.string().optional(),
+            meta: z.object({})
+        })
+        .array()
 });
