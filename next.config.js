98632abf0fea084cdf6f0659f9aad96749ddec42
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-11-09 15:56:07 +0100

    :tada:

--- next.config.js
+++ next.config.js
@@ -2,20 +2,23 @@
 const path = require("path");
 const { NormalModuleReplacementPlugin } = require("webpack");
 const nextConfig = {
-    webpack: (
-        config,
-        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-    ) => {
-        config.plugins = config.plugins || [];
-        config.plugins.push(
-            new NormalModuleReplacementPlugin(
-                /email\/render/,
-                path.resolve(__dirname, "./renderEmailFix.js")
-            )
-        );
-        // Important: return the modified config
-        return config;
+    experimental: {
+        serverComponentsExternalPackages: ["puppeteer-core"]
     },
+    // webpack: (
+    //     config,
+    //     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    // ) => {
+    //     config.plugins = config.plugins || [];
+    //     config.plugins.push(
+    //         new NormalModuleReplacementPlugin(
+    //             /email\/render/,
+    //             path.resolve(__dirname, "./renderEmailFix.js")
+    //         )
+    //     );
+    //     // Important: return the modified config
+    //     return config;
+    // },
     async headers() {
         return [
             {
