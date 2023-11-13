4b5d82585596b7c396d9dbda9c4f80e1a835ee75
ishaqyusuf <ishaqyusuf024@gmail.com>
2023-11-13 16:43:05 +0100

    :tada:

--- components/forms/sales-form/sales-form.tsx
+++ components/forms/sales-form/sales-form.tsx
@@ -55,6 +55,43 @@ export default function SalesForm({ data, newTitle, slug }: Props) {
     });
     const router = useRouter();
 
+    useEffect(() => {
+        const confirmationMessage = "Are you sure you want to leave this page?";
+
+        const handleBeforeUnload = e => {
+            e.preventDefault();
+            e.returnValue = confirmationMessage;
+            return confirmationMessage;
+        };
+        const handleClick = e => {
+            console.log(">>>>>");
+            e.preventDefault();
+            const alert = confirm(
+                "You may have some unsaved changes, Are you sure you want to proceed?"
+            );
+            console.log(alert);
+            if (alert) router.push(e.target.getAttribute("href"));
+
+            // window.removeEventListener("beforeunload", handleBeforeUnload);
+            // You may perform additional actions before redirecting
+            // router.push(e.target.getAttribute("href"));
+        };
+
+        const links = document.querySelectorAll("a");
+
+        links.forEach(link => {
+            link.addEventListener("click", handleClick);
+        });
+
+        window.addEventListener("beforeunload", handleBeforeUnload);
+
+        return () => {
+            links.forEach(link => {
+                link.removeEventListener("click", handleClick);
+            });
+            window.removeEventListener("beforeunload", handleBeforeUnload);
+        };
+    }, [data]);
     // const router = useRouter();
     // const handleClick = e => {
     //     console.log("REQ");
