8d63aadc4075bc699f6c8b365ee47ea7775f183d
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-01-10 21:18:24 +0100

    :tada:

--- app/(v1)/(auth)/sales/estimates/page.tsx
+++ app/(v1)/(auth)/sales/estimates/page.tsx
@@ -25,7 +25,7 @@ export default async function DeliveryPage({ searchParams }) {
             <PageHeader
                 title="Sales Estimates"
                 permissions={["editOrders"]}
-                newLink="/sales/edit/order/new"
+                newLink="/sales/edit/estimate/new"
             />
             <EstimatesTableShell<ISalesOrder>
                 searchParams={searchParams}
