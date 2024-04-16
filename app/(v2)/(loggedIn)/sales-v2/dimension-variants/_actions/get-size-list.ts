fe7d35cec78793f97ed884ac39a4d25573aaac05
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-04-16 16:54:59 +0100

    :tada:

--- app/(v2)/(loggedIn)/sales-v2/dimension-variants/_actions/get-size-list.ts
+++ app/(v2)/(loggedIn)/sales-v2/dimension-variants/_actions/get-size-list.ts
@@ -19,7 +19,20 @@ export async function getDimensionSizeList(height) {
                 width: size.ft,
             });
     });
-    return list.sort((a, b) =>
-        b.width.toLowerCase() > a.width.toLowerCase() ? -1 : 1
-    );
+    return list.sort((a, b) => {
+        // Split each element of the array by '-' to separate the numbers
+        let [aFirst, aSecond] = a.width.split("-").map(Number) as any;
+        let [bFirst, bSecond] = b.width.split("-").map(Number) as any;
+
+        // Compare the first numbers
+        if (aFirst !== bFirst) {
+            return aFirst - bFirst;
+        }
+
+        // If the first numbers are equal, compare the second numbers
+        return aSecond - bSecond;
+    });
+    // return list.sort((a, b) =>
+    //     b.width.toLowerCase() > a.width.toLowerCase() ? -1 : 1
+    // );
 }
