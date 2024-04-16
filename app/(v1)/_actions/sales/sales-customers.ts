4b76c81311c94022f6f0a0d867ceb327cd2e7189
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-04-16 12:57:57 +0100

    a

--- app/(v1)/_actions/sales/sales-customers.ts
+++ app/(v1)/_actions/sales/sales-customers.ts
@@ -48,6 +48,7 @@ export async function getCustomersAction(query: IGetCustomerActionQuery) {
                     salesOrders: {
                         where: {
                             deletedAt: null,
+                            type: "order",
                         },
                     },
                 },
