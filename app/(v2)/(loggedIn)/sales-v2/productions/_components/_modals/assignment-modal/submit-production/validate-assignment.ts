3d7514694152b7295136ca20978eb3d6aa3b520e
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-05-06 17:31:46 +0100

    :tada:

--- app/(v2)/(loggedIn)/sales-v2/productions/_components/_modals/assignment-modal/submit-production/validate-assignment.ts
+++ app/(v2)/(loggedIn)/sales-v2/productions/_components/_modals/assignment-modal/submit-production/validate-assignment.ts
@@ -1,10 +1,15 @@
 import { UseFormReturn } from "react-hook-form";
-import { IAssignGroupForm } from ".";
+
 import { z } from "zod";
 import { toast } from "sonner";
-import { OrderItemProductionAssignments } from "@prisma/client";
+import {
+    OrderItemProductionAssignments,
+    OrderProductionSubmissions,
+} from "@prisma/client";
 
-export function useValidateAssignment(form: UseFormReturn<IAssignGroupForm>) {
+export function useValidateAssignment(
+    form: UseFormReturn<Partial<OrderProductionSubmissions>>
+) {
     return {
         validate() {
             const v = form.getValues();
@@ -12,57 +17,57 @@ export function useValidateAssignment(form: UseFormReturn<IAssignGroupForm>) {
             let schema: any = null;
             form.clearErrors();
             const assignments: Partial<OrderItemProductionAssignments>[] = [];
-            Object.entries(v.doors).map(([title, door]) => {
-                let _assignValidator: any = null;
-                ["lh", "rh"].map((k) => {
-                    const qtyKey = `${k}Qty`;
+            // Object.entries(v.doors).map(([title, door]) => {
+            //     let _assignValidator: any = null;
+            //     ["lh", "rh"].map((k) => {
+            //         const qtyKey = `${k}Qty`;
 
-                    const qty = parseFloat(door._assignForm[qtyKey]);
-                    console.log(qty);
+            //         const qty = parseFloat(door._assignForm[qtyKey]);
+            //         console.log(qty);
 
-                    if (!isNaN(qty) && qty > 0) {
-                        (v.doors[title] as any)._assignForm[qtyKey] =
-                            qty as any;
+            //         if (!isNaN(qty) && qty > 0) {
+            //             (v.doors[title] as any)._assignForm[qtyKey] =
+            //                 qty as any;
 
-                        if (!_assignValidator) _assignValidator = {};
-                        const pending = Number(door?.[`${k}Pending`]) || 0;
-                        _assignValidator[qtyKey] = z
-                            .number({})
-                            .max(pending)
-                            .min(0);
-                        console.log({ qty, pending });
-                    }
-                });
-                if (_assignValidator) {
-                    assignments.push({
-                        ...v.doors?.[title]?._assignForm,
-                        assignedToId: v.assignToId,
-                    });
+            //             if (!_assignValidator) _assignValidator = {};
+            //             const pending = Number(door?.[`${k}Pending`]) || 0;
+            //             _assignValidator[qtyKey] = z
+            //                 .number({})
+            //                 .max(pending)
+            //                 .min(0);
+            //             console.log({ qty, pending });
+            //         }
+            //     });
+            //     if (_assignValidator) {
+            //         assignments.push({
+            //             ...v.doors?.[title]?._assignForm,
+            //             assignedToId: v.assignToId,
+            //         });
 
-                    if (!schema) schema = {};
-                    schema[title] = z.object({
-                        _assignForm: z.object(_assignValidator),
-                    });
-                }
-            });
-            if (schema) {
-                try {
-                    z.object({
-                        doors: z.object(schema),
-                        assignToId: z.number({}).min(1),
-                    }).parse(v);
-                    return assignments;
-                } catch (error) {
-                    (error as any).issues.map((e) => {
-                        console.log(e);
+            //         if (!schema) schema = {};
+            //         schema[title] = z.object({
+            //             _assignForm: z.object(_assignValidator),
+            //         });
+            //     }
+            // });
+            // if (schema) {
+            //     try {
+            //         z.object({
+            //             doors: z.object(schema),
+            //             assignToId: z.number({}).min(1),
+            //         }).parse(v);
+            //         return assignments;
+            //     } catch (error) {
+            //         (error as any).issues.map((e) => {
+            //             console.log(e);
 
-                        form.setError(e.path.join("."), {
-                            ...e,
-                        });
-                    });
-                    return false;
-                }
-            }
+            //             form.setError(e.path.join("."), {
+            //                 ...e,
+            //             });
+            //         });
+            //         return false;
+            //     }
+            // }
             toast.error("Invalid assignment");
             return false;
         },
