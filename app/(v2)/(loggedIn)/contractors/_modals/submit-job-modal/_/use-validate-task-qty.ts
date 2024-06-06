bbfdaa907bf379df0079461315a07aceca9acb6e
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-06-06 14:20:45 +0100

    :tada:

--- app/(v2)/(loggedIn)/contractors/_modals/submit-job-modal/_/use-validate-task-qty.ts
+++ app/(v2)/(loggedIn)/contractors/_modals/submit-job-modal/_/use-validate-task-qty.ts
@@ -32,7 +32,7 @@ export function useValidateTaskQty(form) {
                         v &&
                             typeof (v as any)?.qty === "string" &&
                             (costData[k] = {
-                                ...v,
+                                ...(v as any),
                                 qty: Number((v as any).qty),
                             });
                         // k == "undefined" && delete formData.meta.costData[k];
