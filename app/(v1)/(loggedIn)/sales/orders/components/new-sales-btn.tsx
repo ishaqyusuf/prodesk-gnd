1d4d86ad10f741037b5201f65fac676fb7c84752
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-08-26 15:16:12 +0100

    :tada:

--- app/(v1)/(loggedIn)/sales/orders/components/new-sales-btn.tsx
+++ app/(v1)/(loggedIn)/sales/orders/components/new-sales-btn.tsx
@@ -5,14 +5,33 @@ import {
     MenuItem,
 } from "@/components/_v1/data-table/data-table-row-actions";
 import { Icons } from "@/components/_v1/icons";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function NewSalesBtn({}) {
+    const btns = [
+        { text: "Old", href: "/sales/edit/order/new" },
+        { text: "New", href: "/sales-v2/form/order" },
+    ];
     return (
-        <>
-            <Menu label="New" variant={"default"} Icon={Icons.add}>
+        <div className="flex space-x-2">
+            {btns.map((b) => (
+                <Button
+                    key={b.text}
+                    size="sm"
+                    variant={b.text == "Old" ? "outline" : "default"}
+                    asChild
+                >
+                    <Link href={b.href}>
+                        <Icons.add className="w-4 h-4 mr-2" />
+                        <span>{b.text}</span>
+                    </Link>
+                </Button>
+            ))}
+            {/* <Menu label="New" variant={"default"} Icon={Icons.add}>
                 <MenuItem href={"/sales/edit/order/new"}>Sales Form</MenuItem>
                 <MenuItem href={"/sales-v2/form/order"}>Sales Form 2</MenuItem>
-            </Menu>
-        </>
+            </Menu> */}
+        </div>
     );
 }
--- app/(v1)/(loggedIn)/sales/orders/page.tsx
+++ app/(v1)/(loggedIn)/sales/orders/page.tsx
@@ -1,7 +1,6 @@
 import { getSalesOrder } from "@/app/(v1)/(loggedIn)/sales/_actions/sales";
 
 import { queryParams } from "@/app/(v1)/_actions/action-utils";
-import { ISalesOrder } from "@/types/sales";
 import OrderPrinter from "@/components/_v1/print/order/order-printer";
 import { Metadata } from "next";
 import PageHeader from "@/components/_v1/page-header";
@@ -10,7 +9,6 @@ import { BreadLink } from "@/components/_v1/breadcrumbs/links";
 import SalesTabLayout from "@/components/_v1/tab-layouts/sales-tab-layout";
 import { _restoreSalesOrder } from "@/app/(v1)/_actions/fix/restore-sales-order";
 import { _mergeConflictCustomers } from "@/app/(v1)/_actions/fix/merge-conflict-customer";
-import BackOrderModal from "@/components/_v1/modals/sales/back-order-modal";
 
 import NewSalesBtn from "./components/new-sales-btn";
 import CopyFn from "./components/copy-fn";
