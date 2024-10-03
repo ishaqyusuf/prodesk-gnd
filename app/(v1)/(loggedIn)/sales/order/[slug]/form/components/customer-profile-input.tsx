e1c6a891103b1ca5e5ab24d5c32a0d59d0601dc5
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-10-03 22:11:43 +0100

    :tada:

--- app/(v1)/(loggedIn)/sales/order/[slug]/form/components/customer-profile-input.tsx
+++ app/(v1)/(loggedIn)/sales/order/[slug]/form/components/customer-profile-input.tsx
@@ -1,50 +1,55 @@
 "use client";
 
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select";
 import { ISalesOrderForm } from "@/types/sales";
 import { CustomerTypes } from "@prisma/client";
 
 export function SalesCustomerProfileInput({
-  form,
-  profiles,
+    form,
+    profiles,
 }: {
-  form: ISalesOrderForm;
-  profiles: CustomerTypes[];
+    form: ISalesOrderForm;
+    profiles: CustomerTypes[];
 }) {
-  const profile = form.watch("meta.sales_profile");
+    const profileId = form.watch("customerProfileId");
 
-  return (
-    <div className="flex justify-end">
-      <Select
-        value={profile}
-        onValueChange={(value) => {
-          const selection = profiles.find((profile) => profile.title == value);
-          if (selection) {
-            form.setValue("meta.sales_profile", value);
-            form.setValue("meta.sales_percentage", selection.coefficient);
-          }
-        }}
-      >
-        <SelectTrigger className="h-6 min-w-[100px]">
-          <SelectValue placeholder="Profile" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {profiles?.map((profile, _) => (
-              <SelectItem key={_} value={profile.title}>
-                {profile.title}
-              </SelectItem>
-            ))}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-    </div>
-  );
+    return (
+        <div className="flex justify-end">
+            <Select
+                value={profileId as any}
+                onValueChange={(value) => {
+                    const selection = profiles.find(
+                        (profile) => profile.title == value
+                    );
+                    if (selection) {
+                        //   form.setValue("meta.sales_profile", value);
+                        form.setValue(
+                            "meta.sales_percentage",
+                            selection.coefficient
+                        );
+                    }
+                }}
+            >
+                <SelectTrigger className="h-6 min-w-[100px]">
+                    <SelectValue placeholder="Profile" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        {profiles?.map((profile, _) => (
+                            <SelectItem key={_} value={profile.title}>
+                                {profile.title}
+                            </SelectItem>
+                        ))}
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+        </div>
+    );
 }
