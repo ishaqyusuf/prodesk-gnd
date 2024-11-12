e6d4da3b2fc4d79d887d6dafc06be67c041b04f9
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-11-12 16:39:47 +0100

    :tada:

--- app/(clean-code)/(sales)/sales-book/(pages)/orders/page.tsx
+++ app/(clean-code)/(sales)/sales-book/(pages)/orders/page.tsx
@@ -10,8 +10,8 @@ import { __isProd } from "@/lib/is-prod-server";
 // export const dynamic = "force-dynamic";
 export default async function SalesBookPage({ searchParams }) {
     // const promise = getSalesOrderListUseCase(searchParams);
-    // if (Object.keys(searchParams).length == 0 && __isProd)
-    //     redirect("/sales-book/orders?digest=");
+    if (Object.keys(searchParams).length == 0 && __isProd)
+        redirect("/sales-book/orders?digest=");
 
     const search = searchParamsCache.parse(searchParams);
     const queryClient = getQueryClient();
