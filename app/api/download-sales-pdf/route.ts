9033db86b38e210f17f46155b5bfcdb81576d0d4
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-11-12 14:52:46 +0100

    :tada:

--- app/api/download-sales-pdf/route.ts
+++ app/api/download-sales-pdf/route.ts
@@ -5,10 +5,10 @@ export async function GET(req: Request) {
     const data = await req.json();
 
     const resp = await printSalesPdf(data.mode, data.ids);
-    return NextResponse.json(resp.pdf, {
+    return new NextResponse(resp.pdf, {
         headers: {
             "Content-Type": "application/pdf",
-            "Content-Disposition": 'attachment; filename="invoice.pdf"',
+            "Content-Disposition": `attachment; filename="${resp.fileName}"`,
         },
     });
 }
