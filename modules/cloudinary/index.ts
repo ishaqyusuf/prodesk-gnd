c4c25bdc33eef19c0b1a0c52f291137ec4246ead
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-11-13 15:47:58 +0100

    :tada:

--- modules/cloudinary/index.ts
+++ modules/cloudinary/index.ts
@@ -3,14 +3,6 @@ import { cloudinary } from "./lib";
 
 export type UploadFolders = "sales-orders" | "contractor-document" | "dyke";
 
-export function generateDownloadLink(publicId) {
-    const downloadUrl = cloudinary.url(publicId, {
-        resource_type: "raw",
-        secure: true,
-        // flags: "attachment",
-    });
-    return downloadUrl;
-}
 export async function uploadPDFToCloudinary(
     buffer: Buffer,
     public_id: string,
@@ -39,9 +31,7 @@ export async function uploadPDFToCloudinary(
                                 //     )}media_explorer_thumbnails/${
                                 //     result.asset_id
                                 // }/download`,
-                                downloadUrl: generateDownloadLink(
-                                    result.public_id
-                                ),
+                                downloadUrl: result.secure_url,
                             });
                         } else {
                             reject(error);
