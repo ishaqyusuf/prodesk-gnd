77774bed70ef54bba9558c29b7c1aad5a85e1889
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-11-14 15:18:33 +0100

    :tada:

--- app/(clean-code)/(sales)/sales-book/(pages)/orders/page.tsx
+++ app/(clean-code)/(sales)/sales-book/(pages)/orders/page.tsx
@@ -10,14 +10,13 @@ import { __isProd } from "@/lib/is-prod-server";
 // export const dynamic = "force-dynamic";
 export default async function SalesBookPage({ searchParams }) {
     // const promise = getSalesOrderListUseCase(searchParams);
-    if (Object.keys(searchParams).length == 0 && __isProd)
-        redirect("/sales-book/orders?digest=");
-
-    const search = searchParamsCache.parse(searchParams);
-    const queryClient = getQueryClient();
-    await queryClient.prefetchInfiniteQuery(
-        dataOptions(search, getSalesOrderInfinityListUseCase)
-    );
+    // if (Object.keys(searchParams).length == 0 && __isProd)
+    // redirect("/sales-book/orders?digest=");
+    // const search = searchParamsCache.parse(searchParams);
+    // const queryClient = getQueryClient();
+    // await queryClient.prefetchInfiniteQuery(
+    //     dataOptions(search, getSalesOrderInfinityListUseCase)
+    // );
     return (
         <FPage className="" title="Orders">
             <OrdersPageClient searchParams={searchParams} />
