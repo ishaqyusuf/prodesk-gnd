28acd3ab7e228637ecb7679bb06ca9aa7ec3b5f3
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-11-19 14:55:50 +0100

    :tada:

--- app/(clean-code)/(sales)/_common/use-case/sales-list-use-case.ts
+++ app/(clean-code)/(sales)/_common/use-case/sales-list-use-case.ts
@@ -1,5 +1,8 @@
 "use server";
-import { searchParamsCache } from "@/components/(clean-code)/data-table/search-params";
+import {
+    searchParamsCache,
+    SearchParamsType,
+} from "@/components/(clean-code)/data-table/search-params";
 import { GetSalesListQuery, getSalesOrdersDta } from "../data-access/sales-dta";
 import { AsyncFnType } from "@/app/(clean-code)/type";
 import { deepCopy } from "@/lib/deep-copy";
@@ -7,15 +10,15 @@ import { unstable_noStore } from "next/cache";
 
 export type GetSalesOrderList = AsyncFnType<typeof getSalesOrderListUseCase>;
 export type GetSalesOrderListItem = GetSalesOrderList["data"][number];
-export async function getSalesOrderListUseCase(query: GetSalesListQuery) {
-    query._type = "order";
+export async function getSalesOrderListUseCase(query: SearchParamsType) {
+    // query._type = "order";
     const list = await getSalesOrdersDta(query);
     return list;
 }
 export async function getSalesListByIdUseCase(id) {
     const data = await getSalesOrderListUseCase({
         id,
-    });
+    } as any);
     const res = data?.data?.[0];
     if (!res) throw new Error("Not found");
     return res;
--- data-acces/query-tab-data-access.ts
+++ data-acces/query-tab-data-access.ts
@@ -1,5 +1,11 @@
 import { prisma } from "@/db";
+import { Prisma } from "@prisma/client";
 
 export async function loadQueryTabsDta() {
     const tabs = await prisma.pageTabs.findMany({});
+}
+export async function saveQueryDta(data: Prisma.PageTabsCreateInput) {
+    const resp = await prisma.pageTabs.create({
+        data,
+    });
 }
