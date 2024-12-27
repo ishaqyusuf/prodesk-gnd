8406a694cd9261632caefb917e4b38e1ea35803b
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-12-27 18:16:36 +0100

    :tada:

--- app/(v1)/(loggedIn)/sales/quotes/page.tsx
+++ app/(v1)/(loggedIn)/sales/quotes/page.tsx
@@ -9,6 +9,8 @@ import SalesTabLayout from "@/components/_v1/tab-layouts/sales-tab-layout";
 import AuthGuard from "@/app/(v2)/(loggedIn)/_components/auth-guard";
 import { Metadata } from "next";
 import EstimatesTableShell from "./estimates-table-shell";
+import Portal from "@/components/_v1/portal";
+import NewFeatureBtn from "@/components/common/new-feature-btn";
 
 export const metadata: Metadata = {
     title: "Sales Quote",
@@ -25,7 +27,13 @@ export default async function SalesEstimatesPage({ searchParams }) {
                     <BreadLink isFirst title="Sales" />
                     <BreadLink isLast title="Quotes" />
                 </Breadcrumbs>
-
+                <Portal nodeId={"actionNav"}>
+                    <div>
+                        <NewFeatureBtn href="/sales-book/quotes">
+                            New Site
+                        </NewFeatureBtn>
+                    </div>
+                </Portal>
                 <EstimatesTableShell
                     searchParams={searchParams}
                     {...response}
