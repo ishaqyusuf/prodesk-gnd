1ba4195943acc44b8f2843460e513a997143b14a
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-12-27 21:25:31 +0100

    :tada:

--- components/(clean-code)/data-table/query-options.ts
+++ components/(clean-code)/data-table/query-options.ts
@@ -13,15 +13,12 @@ export type InfiniteQueryMeta = {
 export const dataOptions = (search, queryKey) => {
     return infiniteQueryOptions({
         queryKey: [queryKey, searchParamsSerializer({ ...search, uuid: null })], // remove uuid as it would otherwise retrigger a fetch
+        staleTime: 30 * 1000,
         queryFn: async ({ pageParam = 0 }) => {
             const start = (pageParam as number) * search.size;
             const serialize = searchParamsSerializer({ ...search, start });
             const response = await fetch(
                 `/api/infinite/${queryKey}${serialize}`
-                // {
-                //     cache: {
-                //     }
-                // }
             );
             return response.json();
         },
