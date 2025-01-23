fa1ad221c1bfc9b97e681897f9768d9a4b852d0e
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-01-23 15:02:44 +0100

    :tada:

--- app/(v1)/(loggedIn)/tasks/sales-productions-2/page.tsx
+++ app/(v1)/(loggedIn)/tasks/sales-productions-2/page.tsx
@@ -8,11 +8,13 @@ import { Shell } from "@/components/shell";
 import { _getProductionList } from "@/app/(v2)/(loggedIn)/sales-v2/productions/_components/actions";
 import ProductionList from "@/app/(v2)/(loggedIn)/sales-v2/productions/_components/production-list";
 import PageHeader from "@/components/_v1/page-header";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
     title: "Sales Productions",
 };
 export default function SalesProductionPage({ searchParams }) {
+    redirect(`/sales-book/production-tasks`);
     const p = _getProductionList({ query: searchParams, production: true });
     const dueToday = _getProductionList({
         query: {
