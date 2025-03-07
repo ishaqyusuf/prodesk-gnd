4d2efb12bfb22966ed4ed2419c1bc3a562917020
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-03-07 17:46:13 +0100

    :tada:

---
+++ actions/update-components-sorting.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { prisma } from "@/db";
+
+interface Props {
+    list: {
+        componentId: number;
+        sortUid: string;
+        sortIndex: number;
+    }[];
+}
+export async function updateComponentsSortingAction(data: Props) {
+    await Promise.all(
+        data.list.map(async (ls) => {
+            await prisma.productSortIndex.upsert({
+                create: {
+                    sortIndex: ls.sortIndex,
+                    uid: ls.sortUid,
+                    stepComponentId: ls.componentId,
+                },
+                update: {
+                    sortIndex: ls.sortIndex,
+                },
+                where: {
+                    stepComponentId_uid: {
+                        uid: ls.sortUid,
+                        stepComponentId: ls.componentId,
+                    },
+                },
+            });
+        })
+    );
+}
