b8016fc1b7bd418363b77bfcad617d28ab2859fe
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-03-09 15:41:48 +0100

    :tada:

--- actions/get-sales-checkout-status.ts
+++ actions/get-sales-checkout-status.ts
@@ -1,12 +1,15 @@
 "use server";
 
 import { squareClient, SquarePaymentStatus } from "@/_v2/lib/square";
-import { prisma } from "@/db";
 
 interface Props {
     squareOrderId: string;
+    checkoutId: string;
 }
-export async function getSalesCheckoutStatus({ squareOrderId }: Props) {
+export async function getSalesCheckoutStatus({
+    squareOrderId,
+    checkoutId,
+}: Props) {
     const checkout = await squareClient.ordersApi.retrieveOrder(squareOrderId);
     const resp: { amount; tip; status: SquarePaymentStatus } = {
         amount: null,
